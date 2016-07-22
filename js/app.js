11077f9e1adb09ed6cf2748875d18d03a495e5b6
bnail89 <bnail89@gmail.com>
2016-07-22 05:23:25 -0500

    jquery list works

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-$("#addItem").click(function(){
-    var text = $("#inputName").val() + "<input type='image' id='delete' src='images/trash-can.png' alt='Submit' width='20' height='20'>";
-    if(text.length){
-        $("<li />", {html: text}).appendTo("ul.itemList")
-    }
+$("#addItem").click(function() {
+    var value = $("#inputName").val() + "<input type='image' id='delete' src='images/trash-can.png' alt='Submit' width='20' height='20'>";
+        $("ul").append("<li>" + value + "</li>");
 });
 
 $("ul").on("click","input", function(el){
     $(this).parent().remove()
 });
+
+$("ul").on("click", "li", function(){
+    $(this).wrap("<strike>");
+});
+
