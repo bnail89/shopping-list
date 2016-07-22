3142ce7db42aa56a093b5ae5d08a88cd81a0c855
bnail89 <bnail89@gmail.com>
2016-07-22 03:32:26 -0500

    trash can buttons

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-$('#addItem').click(function(){
-    var text = $('#inputName').val() + '<button>x</button>';
+$("#addItem").click(function(){
+    var text = $("#inputName").val() + "<input type='image' id='delete' src='images/trash-can.png' alt='Submit' width='20' height='20'>";
     if(text.length){
-        $('<li />', {html: text}).appendTo('ul.itemList')
+        $("<li />", {html: text}).appendTo("ul.itemList")
     }
 });
 
-$('ul').on('click','button' , function(el){
+$("ul").on("click","input", function(el){
     $(this).parent().remove()
 });
